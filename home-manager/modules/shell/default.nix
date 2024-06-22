556e07eb8d8b8bc7733416d1650a8bfd36469a91
krabodyan <krabraketa@example.com>
2024-06-22 13:46:24 +0300

    htop

--- home-manager/modules/shell/default.nix
+++ home-manager/modules/shell/default.nix
@@ -3,10 +3,9 @@
     ./fish.nix
     ./python.nix
     ./bat.nix
-    ./btop.nix
+    ./htop.nix
     ./ranger.nix
     ./git.nix
     ./ufetch.nix
   ];
-  home.packages = with pkgs; [ htop nvtopPackages.intel ];
 }
