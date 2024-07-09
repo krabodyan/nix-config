187d02f07b0247e496a5ad9f088f009cf16d25fa
krabodyan <krabraketa@example.com>
2024-07-09 15:40:30 +0300

    change config

--- nixos/system/security/sudo.nix
+++ nixos/system/security/sudo.nix
@@ -2,11 +2,18 @@
   security.sudo.enable = false;
   security.doas = {
     enable = true;
-    extraConfig = ''
-      permit persist keepenv :wheel
-      permit nopass krabodyan as root cmd reboot
-      permit nopass krabodyan as root cmd shutdown
-      permit nopass krabodyan as root cmd intel_gpu_top
-    '';
+    extraRules = [
+      {
+        groups = [ "wheel" ];
+        noPass = false;
+        keepEnv = true;
+        persist = true;
+      }
+      {
+        users = [ "krabodyan" ];
+        cmd = "intel_gpu_top";
+        noPass = true;
+      }
+    ];
   };
 }
