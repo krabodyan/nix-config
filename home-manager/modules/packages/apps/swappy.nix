1762624ea8df1113337c5a340b22a7bcdc429800
krabodyan <krabraketa@example.com>
2024-07-14 17:34:15 +0300

    line size

--- home-manager/modules/packages/apps/swappy.nix
+++ home-manager/modules/packages/apps/swappy.nix
@@ -6,7 +6,7 @@
     save_dir=/home/$USER/pictures
     save_filename_format=swappy-%Y-%m-%d-%H-%M-%S.png
     show_panel=true
-    line_size=5
+    line_size=3
     text_size=20
     text_font=RobotoMedium
     paint_mode=Arrow
