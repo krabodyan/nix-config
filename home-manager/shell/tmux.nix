49693120dfa23771842d0ac524d272416f835212
krabodyan <krabraketa@example.com>
2024-07-30 22:36:04 +0300

    style

--- home-manager/shell/tmux.nix
+++ home-manager/shell/tmux.nix
@@ -5,9 +5,10 @@
     shell = "${pkgs.fish}/bin/fish";
     extraConfig = ''
       set -g prefix C-k
+      set -g status-position top
       set -g update-environment "DISPLAY WAYLAND_DISPLAY SWAYSOCK SSH_AUTH_SOCK"
       set -g default-terminal "tmux-256color"
-      set -sg terminal-overrides ",*:RGB"
+      set -ga terminal-overrides ",*:RGB"
       set -ga terminal-overrides ',*:Ss=\033[4 q:Se=\033[4 q'
     '';
     plugins = [
@@ -23,11 +24,11 @@
           set -g @catppuccin_window_current_text "#W"
           set -g @catppuccin_window_default_color "#{thm_gray}"
 
-          set -g @catppuccin_window_number_position "right"
+          set -g @catppuccin_window_number_position "left"
 
-          set -g @catppuccin_window_left_separator ""
-          set -g @catppuccin_window_middle_separator " | "
-          set -g @catppuccin_window_right_separator " "
+          set -g @catppuccin_window_left_separator "█"
+          set -g @catppuccin_window_middle_separator " "
+          set -g @catppuccin_window_right_separator "█"
 
           set -g @catppuccin_status_background "default"
 
