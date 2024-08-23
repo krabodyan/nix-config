3c83f74bdc7cf89c631ac18ce61e0a733f8c44df
krabodyan <krabraketa@example.com>
2024-08-23 13:48:01 +0300

    commit

--- home-manager/packages/apps/firefox.nix
+++ home-manager/packages/apps/firefox.nix
@@ -28,15 +28,15 @@
         "security.sandbox.content.level" = 0;
         "widget.wayland.opaque-region.enabled" = false;
 
-        "svg.context-properties.content.enabled" = true;
-        "layout.css.color-mix.enabled" = true;
-        "layout.css.light-dark.enabled" = true;
-        "browser.tabs.tabMinWidth" = 66;
-        "browser.tabs.tabClipWidth" = 86;
-        "fp.tweak.autohide-bookmarks" = true;
-        "fp.tweak.macos-button" = false;
-        "fp.tweak.rounded-corners" = true;
-        "fp.tweak.sidebar-enabled" = false;
+        # "svg.context-properties.content.enabled" = true;
+        # "layout.css.color-mix.enabled" = true;
+        # "layout.css.light-dark.enabled" = true;
+        # "browser.tabs.tabMinWidth" = 66;
+        # "browser.tabs.tabClipWidth" = 86;
+        # "fp.tweak.autohide-bookmarks" = true;
+        # "fp.tweak.macos-button" = false;
+        # "fp.tweak.rounded-corners" = true;
+        # "fp.tweak.sidebar-enabled" = false;
 
         "layout.spellcheckDefault" = 0;
         "app.update.auto" = false;
@@ -54,6 +54,14 @@
         "browser.send_pings.require_same_host" = true;
         "toolkit.telemetry.enabled" = false;
         "default-browser-agent.enabled" = false;
+        "onebar.conditional-navigation-buttons" = false;
+        "onebar.disable-centering-of-URLbar" = false;
+        "onebar.disable-https-truncate" = true;
+        "onebar.disable-single-tab" = true;
+        "onebar.hide-all-URLbar-icons" = true;
+        "onebar.hide-all-tabs-button" = true;
+        "onebar.hide-navigation-buttons" = true;
+        "onebar.hide-unified-extensions-button" = true;
         DisableTelemetry = true;
         DisableFirefoxStudies = true;
         DisablePocket = true;
@@ -61,69 +69,7 @@
         DisplayMenuBar = "default-off"; # alternatives: "always", "never" or "default-on"
         SearchBar = "unified"; # alternative: "separate"
       };
-      userChrome = ''
-        /* Source file https://github.com/MrOtherGuy/firefox-csshacks/tree/master/chrome/toolbars_below_content.css made available under Mozilla Public License v. 2.0 */
-
-        @-moz-document url(chrome://browser/content/browser.xhtml){
-          
-          #titlebar{ -moz-appearance: none !important; }
-
-          #navigator-toolbox > div{ display: contents }
-          .global-notificationbox,
-          #mainPopupSet,
-          #browser,
-          #customization-container,
-          #tab-notification-deck{
-            order: -1;
-          }
-
-          #titlebar{
-            order: 2;
-          } 
-
-          #toolbar-menubar{
-            position: fixed;
-            display: flex;
-            width: 100vw;
-            top: 0px;
-            -moz-window-dragging: drag;
-          }
-          #navigator-toolbox{ border-bottom: none !important; }
-
-          #toolbar-menubar > spacer{ flex-grow: 1 }
-
-          #urlbar[breakout][breakout-extend]{
-            display: flex !important;
-            flex-direction: column-reverse;
-            bottom: 0px !important; /* Change to 3-5 px if using compact_urlbar_megabar.css depending on toolbar density */
-            top: auto !important;
-          }
-
-          .urlbarView-body-inner{ border-top-style: none !important; }
-
-          #TabsToolbar > .titlebar-buttonbox-container{ display: none }
-          #toolbar-menubar > .titlebar-buttonbox-container{ order: 1000 }
-
-          .panel-viewstack{ max-height: unset !important; }
-
-          :root[sizemode="fullscreen"] #navigator-toolbox{ margin-top: 0 !important }
-          :root[sizemode="fullscreen"] #navigator-toolbox[style*="margin-top"]{ visibility: collapse }
-          #fullscr-toggler{ bottom: 0; top: unset !important; }
-          
-          #navigator-toolbox{ bottom: 0px; transform-origin: bottom }
-          #main-window > body > box{ margin-top: 0 !important; }
-          #toolbar-menubar{ z-index: 1; background-color: var(--lwt-accent-color,black); }
-          
-          :root[BookmarksToolbarOverlapsBrowser] #navigator-toolbox{
-            margin-block: calc(-1 * var(--bookmarks-toolbar-height)) 0 !important;
-          }
-          :root[BookmarksToolbarOverlapsBrowser] .newTabBrowserPanel{
-            padding-block: 0 var(--bookmarks-toolbar-height) !important;
-          }
-          
-          #titlebar{ flex-direction: column }
-        }
-      '';
+      userChrome = builtins.readFile ../../../assets/firefox.css;
     };
   };
 }
