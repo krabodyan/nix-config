92475a3c55b90e2b5c547ebe9b0d938773bdcba1
krabodyan <krabraketa@example.com>
2024-09-02 10:29:29 +0300

    firefox bar

--- home-manager/packages/apps/firefox.nix
+++ home-manager/packages/apps/firefox.nix
@@ -1,5 +1,6 @@
 { pkgs, ... }:
 {
+  home.packages = [ pkgs.chromium ];
   programs.firefox = {
     enable = true;
     package = pkgs.firefox-wayland;
@@ -7,6 +8,7 @@
       settings = {
         "general.autoScroll" = false;
         "browser.urlbar.autoFill" = false;
+        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
         "media.webspeech.synth.enabled" = false;
         "privacy.webrtc.legacyGlobalIndicator" = false;
         "ui.key.menuAccessKeyFocuses" = false;
@@ -23,14 +25,14 @@
         "browser.newtabpage.activity-stream.showSponsored" = false;
         "browser.newtabpage.activity-stream.system.showSponsored" = false;
         "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
-        "media.ffmpeg.vaapi.enabled" = true;
-        "media.ffmpeg.encoder.enabled" = true;
-        "security.sandbox.content.level" = 0;
-        "widget.wayland.opaque-region.enabled" = false;
+        # "media.ffmpeg.vaapi.enabled" = true;
+        # "media.ffmpeg.encoder.enabled" = true;
+        # "security.sandbox.content.level" = 0;
+        # "widget.wayland.opaque-region.enabled" = false;
 
-        "svg.context-properties.content.enabled" = true;
-        "layout.css.color-mix.enabled" = true;
-        "layout.css.light-dark.enabled" = true;
+        # "svg.context-properties.content.enabled" = true;
+        # "layout.css.color-mix.enabled" = true;
+        # "layout.css.light-dark.enabled" = true;
         "browser.tabs.tabMinWidth" = 66;
         "browser.tabs.tabClipWidth" = 86;
 
@@ -40,17 +42,18 @@
         "browser.tabs.insertAfterCurrent" = true;
         "browser.in-content.dark-mode" = true;
         "ui.systemUsesDarkTheme" = 1;
-        "browser.startup.page" = 3;
+        # "browser.startup.page" = 3;
         "datareporting.healthreport.uploadEnabled" = false;
-        "media.peerconnection.enabled" = false;
+        # "media.peerconnection.enabled" = false;
         "browser.search.suggest.enabled.private" = true;
         "geo.enabled" = false;
         "geo.wifi.uri" = "";
         "browser.search.geoip.url" = "";
-        "browser.send_pings.require_same_host" = true;
+        # "browser.send_pings.require_same_host" = true;
         "toolkit.telemetry.enabled" = false;
-        "default-browser-agent.enabled" = false;
-        "onebar.conditional-navigation-buttons" = false;
+        # "default-browser-agent.enabled" = false;
+
+        # "onebar.conditional-navigation-buttons" = false;
         "onebar.disable-centering-of-URLbar" = false;
         "onebar.disable-https-truncate" = true;
         "onebar.disable-single-tab" = true;
@@ -58,10 +61,14 @@
         "onebar.hide-all-tabs-button" = true;
         "onebar.hide-navigation-buttons" = true;
         "onebar.hide-unified-extensions-button" = false;
+
+        "browser.warnOnQuit" = false;
+        "browser.warnOnQuitShortcut" = false;
+        "browser.download.always_ask_before_handling_new_types" = true;
         DisableTelemetry = true;
         DisableFirefoxStudies = true;
         DisablePocket = true;
-        DisplayBookmarksToolbar = "never"; # alternatives: "always" or "newtab"
+        DisplayBookmarksToolbar = "always"; # alternatives: "always" or "newtab"
         DisplayMenuBar = "default-off"; # alternatives: "always", "never" or "default-on"
         SearchBar = "unified"; # alternative: "separate"
       };
