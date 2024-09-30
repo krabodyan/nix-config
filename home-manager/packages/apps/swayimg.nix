fcb2cd4f920968ba1fa68fe09cbdd098ae5df9d7
krabodyan <krabraketa@example.com>
2024-09-30 12:39:38 +0300

    commit

--- home-manager/packages/apps/swayimg.nix
+++ home-manager/packages/apps/swayimg.nix
@@ -8,35 +8,48 @@
   home.packages = [ pkgs.swayimg ];
   xdg.configFile."swayimg/config".text = with config.colors; ''
     [general]
+    size = fullscreen
+
+    [viewer]
     scale = optimal
-    fullscreen = yes
     transparency = grid
-    background = #${bg}
+    window = #${bg}ff
     slideshow = no
 
     [font]
     name = ${config.font}
     size = 14
-    color = #${fg}00
+    color = #${fg}ff
     shadow = #000000a0
 
-    [keys]
+    [keys.viewer]
+    k = prev_file
+    j = next_file
     left = rotate_left
     right = rotate_right
     bracketleft = flip_vertical
     bracketright = flip_horizontal
     up = prev_file
     down = next_file
-
-    [info]
-    mode = brief
-    brief.topleft = name
-
-    [mouse]
     Shift+ScrollUp = step_down 5
     Shift+ScrollDown = step_up 5
     ScrollUp = zoom +10
     ScrollDown = zoom -10
+
+    [info]
+    show = yes
+
+    [info.gallery]
+    top_left = +name,+format,+filesize,+imagesize,+exif
+    top_right = index
+    bottom_left = scale,frame
+    bottom_right = status
+
+    [info.viewer]
+    top_left = name,status
+    top_right = none
+    bottom_right = none
+    bottom_left = none
 
     [list]
     order = alpha
