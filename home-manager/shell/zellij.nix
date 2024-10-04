44ea5d6cb606d32fe446a307eb7645c565ed3af2
krabodyan <krabraketa@example.com>
2024-10-04 15:04:35 +0300

    commit

--- home-manager/shell/zellij.nix
+++ home-manager/shell/zellij.nix
@@ -20,17 +20,42 @@
       }
     }
 
-    keybinds {
-      unbind "Alt l" "Alt h" "Ctrl s"
+    keybinds clear-defaults=true {
       normal {
-        bind "Alt i" { SwitchToMode "tab"; }
-        bind "Ctrl j" { SwitchToMode "scroll"; }
+        bind "Alt r" { SwitchToMode "RenameTab"; TabNameInput 0; }
       }
-      tab {
-        bind "Ctrl c" { SwitchToMode "normal"; }
-        bind "h" "Left" { MoveFocusOrTab "Left"; }
-        bind "l" "Right" { MoveFocusOrTab "Right"; }
-        bind "p" { SwitchFocus; }
+      
+      scroll {
+        bind "Ctrl j" { SwitchToMode "normal"; }
+        bind "j" "Down" { ScrollDown; }
+        bind "k" "Up" { ScrollUp; }
+      }
+      
+      renametab {
+        bind "Esc" "Ctrl c" { UndoRenameTab; SwitchToMode "normal"; }
+      }
+      
+      shared_except "normal" "locked" {
+        bind "Enter" "Esc" { SwitchToMode "Normal"; }
+      }
+      
+      shared_except "locked" {
+        bind "Esc" { SwitchToMode "normal"; }
+        bind "Ctrl q" { Quit; }
+        bind "Alt c" { NewTab; }
+        bind "Alt x" { CloseTab; SwitchToMode "Normal"; }
+        bind "Ctrl j" { SwitchToMode "scroll"; }
+        bind "Alt 1" { GoToTab 1; SwitchToMode "Normal"; }
+        bind "Alt 2" { GoToTab 2; SwitchToMode "Normal"; }
+        bind "Alt 3" { GoToTab 3; SwitchToMode "Normal"; }
+        bind "Alt 4" { GoToTab 4; SwitchToMode "Normal"; }
+        bind "Alt 5" { GoToTab 5; SwitchToMode "Normal"; }
+        bind "Alt 6" { GoToTab 6; SwitchToMode "Normal"; }
+        bind "Alt 7" { GoToTab 7; SwitchToMode "Normal"; }
+        bind "Alt 8" { GoToTab 8; SwitchToMode "Normal"; }
+        bind "Alt 9" { GoToTab 9; SwitchToMode "Normal"; }
+        bind "Ctrl h" { GoToPreviousTab; }
+        bind "Ctrl l" { GoToNextTab; }
       }
     }
 
