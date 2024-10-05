f1b32ed16a190bf97842280043ee64ed2f048036
krabodyan <krabraketa@example.com>
2024-10-06 00:00:42 +0300

    commit

--- home-manager/packages/apps/mpv.nix
+++ home-manager/packages/apps/mpv.nix
@@ -25,14 +25,14 @@
   programs.mpv = {
     enable = true;
     extraInput = ''
-      WHEEL_UP     add volume  5
-      WHEEL_DOWN   add volume -5
+      WHEEL_UP     add volume  3
+      WHEEL_DOWN   add volume -3
       h            seek       -3
       l            seek        3
       k            add volume  5
       j            add volume -5
-      UP           add volume  5
-      DOWN         add volume -5
+      UP           add volume  3
+      DOWN         add volume -3
       RIGHT        seek        3
       LEFT         seek       -3
       s            ignore
