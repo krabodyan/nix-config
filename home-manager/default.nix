fc8749d9a8dfd932eea52850dc3a27597f9fcfad
krabodyan <krabraketa@example.com>
2024-11-18 23:55:32 +0200

    commit

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -3,7 +3,6 @@
     username = "krabodyan";
     homeDirectory = "/home/krabodyan";
     stateVersion = "25.05";
-    enableNixpkgsReleaseCheck = false;
   };
   programs.home-manager.enable = true;
   news.display = "silent";
