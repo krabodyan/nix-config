6949c0823470b4b7751fa0faa27d18b73f993b68
krabodyan <krabraketa@example.com>
2025-01-19 15:28:29 +0200

    commit

--- nixos/system/security/default.nix
+++ nixos/system/security/default.nix
@@ -1,13 +1,11 @@
 {
   imports = [ ./sudo.nix ];
-  security.pam.loginLimits = [
-    {
-      domain = "krabodyan";
-      type = "soft";
-      item = "core";
-      value = "unlimited";
-    }
-  ];
+  security.pam.loginLimits = [{
+    domain = "krabodyan";
+    type = "soft";
+    item = "core";
+    value = "unlimited";
+  }];
 
   security.pam.services.swaylock = { };
 }
--- nixos/system/services/docker.nix
+++ nixos/system/services/docker.nix
@@ -1,8 +1,8 @@
-{ lib, ... }:
-{
+{ lib, ... }: {
   virtualisation.docker = {
     enable = true;
     enableOnBoot = false;
+    storageDriver = "btrfs";
   };
   systemd.services.docker.wantedBy = lib.mkForce [ ];
   systemd.sockets.docker.wantedBy = lib.mkForce [ ];
