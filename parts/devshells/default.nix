5c38e60207c53666f3508723e1b2b952ed49bb39
krabodyan <krabraketa@gmail.com>
2025-03-18 21:49:47 +0200

    remove shellHook + add light rust shell

--- parts/devshells/default.nix
+++ parts/devshells/default.nix
@@ -4,13 +4,36 @@
       inherit (pkgs) system;
       overlays = [(import inputs.rust-overlay)];
     };
-    shellHook = ''test -n "$TMUX" || exec tmux'';
   in {
     devShells.rust = pkgs.mkShell {
-      inherit shellHook;
       name = "rust";
-      meta.description = "DevShell for Rust with Tauri | Dioxus";
+      meta.description = "Light DevShell for Rust";
       DEV_SHELL_NAME = "rust";
+      RUST_BACKTRACE = 1;
+
+      nativeBuildInputs = with pkgs; [
+        cargo-watch
+        cargo-expand
+        pkg-config
+        # sqlite
+        # openssl
+      ];
+
+      buildInputs = with pkgs; [
+        libudev-zero
+        (
+          rust-pkgs.rust-bin.nightly.latest.default.override
+          {
+            targets = ["x86_64-unknown-linux-gnu"];
+            extensions = ["rust-src" "rust-analyzer"];
+          }
+        )
+      ];
+    };
+    devShells.tauri = pkgs.mkShell {
+      name = "tauri";
+      meta.description = "DevShell for Rust with Tauri";
+      DEV_SHELL_NAME = "tauri";
       RUST_BACKTRACE = 1;
       GIO_MODULE_DIR = "${pkgs.glib-networking}/lib/gio/modules/";
 
@@ -51,14 +74,13 @@
           rust-pkgs.rust-bin.nightly.latest.default.override
           {
             targets = ["wasm32-unknown-unknown" "x86_64-unknown-linux-gnu"];
-            extensions = ["rust-src" "rust-analyzer" "miri"];
+            extensions = ["rust-src" "rust-analyzer"];
           }
         )
       ];
     };
     devShells.ino = pkgs.mkShell {
       DEV_SHELL_NAME = "ino";
-      inherit shellHook;
       buildInputs = [pkgs.glibc_multi];
     };
   };
