01cf59cbce3e84dbd06b55e42ee80e9eaf7fe42d
krabodyan <krabraketa@gmail.com>
2025-03-18 22:55:13 +0200

    workaround for pipewire 1.4 + other changes

--- modules/home/gui/mpv/default.nix
+++ modules/home/gui/mpv/default.nix
@@ -15,6 +15,43 @@ in {
     };
   };
   config = mkIf cfg.enable {
+    xdg.configFile."mpv/mpv.conf".text = with colors.hex; ''
+      gpu-context=wayland
+      vo=dmabuf-wayland
+      ao=pulse                # todo: remove after fix
+      volume=50
+      keep-open=yes
+      fullscreen=no
+      force-window
+      sid=0
+
+      audio-file-auto-exts=mkv
+      audio-file-auto=fuzzy
+      audio-file-paths=RUS Sound
+
+      osd-font='${systemFont}'
+      osd-font-size=40
+      osd-border-color='${surface1}'
+      osd-border-size=1
+      osd-color='${fg-bright}'
+      osd-shadow-color='${bg}'
+    '';
+    programs.mpv = {
+      enable = true;
+      extraInput = ''
+        WHEEL_UP     add volume  3
+        WHEEL_DOWN   add volume -3
+        h            seek       -3
+        l            seek        3
+        k            add volume  5
+        j            add volume -5
+        UP           add volume  3
+        DOWN         add volume -3
+        RIGHT        seek        3
+        LEFT         seek       -3
+        s            ignore
+      '';
+    };
     xdg.mimeApps.defaultApplications = mkAssociations {
       types = [
         "video/3gp"
@@ -70,45 +107,6 @@ in {
         "audio/x-wav"
       ];
       desktop = "mpv.desktop";
-    };
-
-    xdg.configFile."mpv/mpv.conf".text = with colors.hex; ''
-      gpu-context=wayland
-      vo=gpu
-      profile=fast
-      hwdec=vaapi
-      volume=50
-      keep-open=yes
-      fullscreen=no
-      force-window
-      sid=0
-
-      audio-file-auto-exts=mkv
-      audio-file-auto=fuzzy
-      audio-file-paths=RUS Sound
-
-      osd-font='${systemFont}'
-      osd-font-size=40
-      osd-border-color='${overlay0}'
-      osd-border-size=1
-      osd-color='${fg-bright}'
-      osd-shadow-color='${bg}'
-    '';
-    programs.mpv = {
-      enable = true;
-      extraInput = ''
-        WHEEL_UP     add volume  3
-        WHEEL_DOWN   add volume -3
-        h            seek       -3
-        l            seek        3
-        k            add volume  5
-        j            add volume -5
-        UP           add volume  3
-        DOWN         add volume -3
-        RIGHT        seek        3
-        LEFT         seek       -3
-        s            ignore
-      '';
     };
   };
 }
