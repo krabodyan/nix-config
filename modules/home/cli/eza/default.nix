986cfc13b73925048ad361c93c7d367544494265
krabodyan <krabraketa@gmail.com>
2025-03-25 12:10:05 +0200

    chore(eza): minor alias changes

--- modules/home/cli/eza/default.nix
+++ modules/home/cli/eza/default.nix
@@ -17,8 +17,13 @@ in {
       eza = "${pkgs.eza}/bin/eza --group-directories-first --long --git";
     in rec {
       l = "${eza} --no-filesize --no-user --no-time --no-permissions";
-      la = "${l} -a";
+
+      lsa = "${l} -a";
       ls = "${l} --git-ignore";
+      la = "${l} -a --git-ignore";
+
+      lt = "${l} --tree --git-ignore";
+
       ll = "${eza} --octal-permissions --no-permissions";
       lla = "${eza} --octal-permissions --no-permissions -a";
     };
