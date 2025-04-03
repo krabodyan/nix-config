a2ccb507358a3f3dc98a0ce8e82d9b37a0bc6e80
krabodyan <krabraketa@gmail.com>
2025-04-03 11:57:21 +0300

    style: htop

--- modules/home/cli/htop/default.nix
+++ modules/home/cli/htop/default.nix
@@ -20,14 +20,14 @@ in {
         color_scheme=5
         cpu_count_from_one=1
         delay=5
-        fields=0 49 46 47 1
+        fields=113 2 0 49 46 47 1
         hide_kernel_threads=1
         hide_userland_threads=1
         highlight_base_name=1
         left_meter_modes=1 2 1 1
         left_meters=LeftCPUs2 Blank CPU Memory
         right_meter_modes=1 2 2 2
-        right_meters=RightCPUs2 Blank Blank LoadAverage
+        right_meters=RightCPUs2 Blank Tasks LoadAverage
         show_cpu_frequency=1
         show_cpu_temperature=1
         show_program_path=0
