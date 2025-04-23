0ab04b39e55dc7c6f6e1ffca6f98845ffbbfca45
krabodyan <krabodyan@gmail.com>
2025-04-23 17:28:30 +0300

    chore(udev): remove probe-rs rules

--- modules/system/udev/default.nix
+++ modules/system/udev/default.nix
@@ -17,12 +17,10 @@ in {
     };
   };
   config = mkIf cfg.enable {
-    services.udev.extraRules =
-      ''
-        KERNEL=="hidraw*", SUBSYSTEM=="hidraw", MODE="0660", TAG+="uaccess"
-        ACTION=="add", SUBSYSTEM=="usb", DRIVERS=="usb", ATTRS{idVendor}=="046d", ATTR{power/wakeup}="disabled"
-      ''
-      + builtins.readFile ./69-probe-rs.rules;
+    services.udev.extraRules = ''
+      KERNEL=="hidraw*", SUBSYSTEM=="hidraw", MODE="0660", TAG+="uaccess"
+      ACTION=="add", SUBSYSTEM=="usb", DRIVERS=="usb", ATTRS{idVendor}=="046d", ATTR{power/wakeup}="disabled"
+    '';
     services.udev.packages =
       mkIf cfg.platformio
       [pkgs.platformio-core.udev];
