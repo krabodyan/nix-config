06f287812b0e81f794be5b19f655771c57736c56
krabodyan <krabodyan@gmail.com>
2025-05-01 21:30:24 +0300

    style(eza): file permissions

--- modules/home/cli/eza/default.nix
+++ modules/home/cli/eza/default.nix
@@ -16,14 +16,11 @@ in {
     programs.fish.shellAliases = let
       eza = "${pkgs.eza}/bin/eza --group-directories-first --long --git --sort ext";
     in rec {
-      l = "${eza} --no-filesize --no-user --no-time --no-permissions";
-
-      ls = "${l}";
-      la = "${l} -a";
-      lt = "${l} --tree";
-
-      ll = "${eza} --octal-permissions --no-permissions";
-      lla = "${eza} --octal-permissions --no-permissions -a";
+      ls = "${eza} --no-filesize --no-user --no-time --no-permissions";
+      la = "${ls} -a";
+      lt = "${ls} --tree";
+      ll = "${eza}";
+      lla = "${eza} -a";
     };
     home.packages = [pkgs.eza];
   };
