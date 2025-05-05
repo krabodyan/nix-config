2b03eeb1a428b8a709f5d69560beb7f346bcc445
krabodyan <krabodyan@gmail.com>
2025-05-05 14:59:38 +0300

    chore: update to new mako hm module

--- modules/home/wm-utils/mako/default.nix
+++ modules/home/wm-utils/mako/default.nix
@@ -18,43 +18,53 @@ in {
     home.packages = [pkgs.libnotify];
     services.mako = with colors.hex; {
       enable = true;
-      font = "${fonts.monospace} SemiBold 11";
-      sort = "-time";
-      layer = "overlay";
-      anchor = "bottom-center";
-      backgroundColor = bg;
-      progressColor = "over ${overlay0}";
-      textColor = fg;
-      width = 400;
-      height = 110;
-      padding = "12,0";
-      borderSize = 2;
-      borderColor = overlay0;
-      borderRadius = 8;
-      maxIconSize = 32;
-      defaultTimeout = 3000;
-      extraConfig = ''
-        icons=0
-        text-alignment=center
-        group-by=app-name
-        outer-margin=32,0
+      settings = {
+        icons = 0;
+        markup = 0;
+        font = "${fonts.monospace} SemiBold 11";
+        sort = "-time";
+        layer = "overlay";
+        group-by = "app-name";
+        anchor = "bottom-center";
 
-        [grouped=1]
-        format=<b>%s [%g]</b>\n%b
+        text-color = fg;
+        background-color = bg;
+        border-color = overlay0;
+        progress-color = "over ${overlay0}";
 
-        [urgency=critical]
-        border-color=${red}
-        default-timeout=12000
+        width = 400;
+        height = 110;
+        border-size = 2;
+        padding = "12,0";
+        border-radius = 8;
+        default-timeout = 3000;
+        text-alignment = "center";
+        outer-margin = "32,0";
+      };
+      criteria = let
+        beep = "exec ${pkgs.pipewire}/bin/pw-cat -p ${pkgs.sound-theme-freedesktop}/share/sounds/freedesktop/stereo/audio-volume-change.oga &";
+      in {
+        "grouped=1" = {
+          format = "<b>%s [%g]</b>\\n%b";
+        };
 
-        [app-name="foot" urgency="normal"]
-        border-color=${green}
+        "urgency=critical" = {
+          border-color = red;
+          default-timeout = 12000;
+        };
 
-        [app-name="foot"]
-        on-notify=exec ${pkgs.pipewire}/bin/pw-cat -p ${pkgs.sound-theme-freedesktop}/share/sounds/freedesktop/stereo/audio-volume-change.oga &
+        "app-name=foot urgency=normal" = {
+          border-color = green;
+        };
 
-        [app-name="swaynotify"]
-        on-notify=exec ${pkgs.pipewire}/bin/pw-cat -p ${pkgs.sound-theme-freedesktop}/share/sounds/freedesktop/stereo/audio-volume-change.oga &
-      '';
+        "app-name=foot" = {
+          on-notify = beep;
+        };
+
+        "app-name=swaynotify" = {
+          on-notify = beep;
+        };
+      };
     };
   };
 }
