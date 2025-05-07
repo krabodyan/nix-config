3d95fc495a12e1f5c68d7c50dd34dfff2f4893fd
krabodyan <krabodyan@gmail.com>
2025-05-07 17:41:50 +0300

    style(nix): remove unused substitudes

--- modules/system/nix-config/default.nix
+++ modules/system/nix-config/default.nix
@@ -45,16 +45,16 @@ in {
 
         substituters = [
           "https://helix.cachix.org"
-          "https://nixpkgs-wayland.cachix.org"
           "https://cache.nixos.org"
-          "https://chaotic-nyx.cachix.org/"
+          # "https://nixpkgs-wayland.cachix.org"
+          # "https://chaotic-nyx.cachix.org/"
         ];
 
         trusted-public-keys = [
           "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
           "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
-          "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
-          "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
+          # "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+          # "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
         ];
 
         trusted-users = [
