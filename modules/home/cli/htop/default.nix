af904f8fa7d46a2a38bcc3c6778df82a865f1085
krabodyan <krabodyan@gmail.com>
2025-05-13 23:41:00 +0300

    style(htop): change tabs

--- modules/home/cli/htop/default.nix
+++ modules/home/cli/htop/default.nix
@@ -18,12 +18,14 @@ in {
       force = true;
       text = ''
         color_scheme=6
+        screen_tabs=1
         cpu_count_from_one=1
         delay=5
-        fields=113 2 0 49 46 47 1
         hide_kernel_threads=1
         hide_userland_threads=1
         highlight_base_name=1
+        highlight_megabytes=0
+        header_margin=0
         left_meter_modes=1 2 1 1
         left_meters=LeftCPUs2 Blank CPU Memory
         right_meter_modes=1 2 2 2
