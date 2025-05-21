e8b3e2ffeeff157f3a466d76303f0063a94708d4
krabodyan <krabodyan@gmail.com>
2025-05-21 21:43:43 +0300

    style(eza): add __pycache__ ignores

--- modules/home/cli/eza/default.nix
+++ modules/home/cli/eza/default.nix
@@ -16,7 +16,7 @@ in {
     programs.fish.shellAliases = let
       eza = "${pkgs.eza}/bin/eza --group-directories-first --long --sort ext";
     in rec {
-      ls = "${eza} --no-filesize --no-user --no-time --no-permissions --ignore-glob \"target|node_modules|\"";
+      ls = "${eza} --no-filesize --no-user --no-time --no-permissions --ignore-glob \"target|node_modules|__pycache__\"";
       la = "${ls} -a";
       lt = "${ls} --tree";
       ll = "${eza} --group";
