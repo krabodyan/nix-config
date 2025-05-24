b01b6f4eaf32ba631af905ec75cf81643ec993d4
krabodyan <krabodyan@gmail.com>
2025-05-24 23:09:02 +0300

    chore(helix): many bind changes, add "F" menu

--- modules/home/cli/helix/binds.nix
+++ modules/home/cli/helix/binds.nix
@@ -1,9 +1,9 @@
 let
   toggle = "A-d";
   binds = {
-    "A-backspace" = ["commit_undo_checkpoint" "delete_word_backward"];
+    A-backspace = ["commit_undo_checkpoint" "delete_word_backward"];
 
-    "A-ret" = [
+    A-ret = [
       "insert_mode"
       "insert_newline"
       "commit_undo_checkpoint"
@@ -12,32 +12,35 @@ let
       "insert_tab"
     ];
 
-    "A-S-h" = "unindent";
-    "A-S-l" = "indent";
+    A-S-h = "unindent";
+    A-S-l = "indent";
 
-    "C-c" = "completion";
+    C-c = "completion";
 
-    "C-k" = "kill_to_line_end";
-    "C-K" = "kill_to_line_start";
-    "A-y" = "save_selection";
-    "A-f" = "goto_line_end_newline";
-    "A-g" = "goto_first_nonwhitespace";
-    "A-a" = ["normal_mode" "goto_word"];
+    C-k = "kill_to_line_end";
+    C-K = "kill_to_line_start";
+    A-y = "save_selection";
+    A-f = "goto_line_end_newline";
+    A-g = "goto_first_nonwhitespace";
+    A-a = ["normal_mode" "goto_word"];
 
-    "A-s" = [":write" "normal_mode" "collapse_selection" "commit_undo_checkpoint"];
-    "A-S" = [":write!" "normal_mode" "collapse_selection" "commit_undo_checkpoint"];
+    A-s = [":write" "normal_mode" "collapse_selection" "commit_undo_checkpoint"];
+    A-S = [":write!" "normal_mode" "collapse_selection" "commit_undo_checkpoint"];
 
-    "A-e" = ":buffer-next";
-    "A-w" = ":buffer-previous";
+    A-e = ":buffer-next";
+    A-w = ":buffer-previous";
 
     "A-/" = "toggle_comments";
     "A-7" = "toggle_comments";
     "A-?" = "toggle_block_comments";
 
-    "A-S-j" = ["extend_to_line_bounds" "delete_selection" "paste_after"];
-    "A-S-k" = ["extend_to_line_bounds" "delete_selection" "move_line_up" "paste_before"];
+    "A-=" = "increment";
+    "A-minus" = "decrement";
 
-    "C-A-k" = [
+    A-S-j = ["extend_to_line_bounds" "delete_selection" "paste_after"];
+    A-S-k = ["extend_to_line_bounds" "delete_selection" "move_line_up" "paste_before"];
+
+    C-A-k = [
       "trim_selections"
       "normal_mode"
       "extend_line"
@@ -48,7 +51,7 @@ let
       "collapse_selection"
     ];
 
-    "C-A-j" = [
+    C-A-j = [
       "trim_selections"
       "normal_mode"
       "extend_line"
@@ -59,10 +62,6 @@ let
       "replace_with_yanked"
       "collapse_selection"
     ];
-
-    "A-=" = "increment";
-    "A-minus" = "decrement";
-    "C-b" = ":echo %sh{ gh browse %{buffer_name}:%{cursor_line} -c=%sh{ git rev-parse HEAD } > /dev/null && echo opening in browser }";
   };
 
   special = {
@@ -77,69 +76,79 @@ let
     t = ["normal_mode" "extend_to_word"];
     a = ["append_mode" "collapse_selection"];
 
-    p = ":clipboard-paste-after";
     P = "paste_before";
-
+    p = ":clipboard-paste-before";
     R = ":clipboard-paste-replace";
 
     y = "yank_to_clipboard";
     Y = "yank";
-    "A-y" = ":clipboard-yank";
 
     i = ["collapse_selection" "insert_mode"];
 
-    J = "join_selections";
-    S = "split_selection_on_newline";
-    "C-j" = "join_selections_space";
-    "S-m" = "merge_selections";
+    M = "merge_selections";
+    J = "join_selections_space";
+    A-J = "join_selections_space";
 
-    "S-c" = "copy_selection_on_next_line";
-    "A-c" = "copy_selection_on_prev_line";
+    S-c = "copy_selection_on_next_line";
+    A-c = "copy_selection_on_prev_line";
 
-    "C-r" = ["insert_mode" "insert_register"];
+    C-r = ["insert_mode" "insert_register"];
 
     esc = ["collapse_selection" "keep_primary_selection"];
+
+    C-b = ":echo %sh{ gh browse %{buffer_name}:%{cursor_line} -c=%sh{ git rev-parse HEAD } > /dev/null && echo opening in browser }";
   };
 in rec {
   insert =
     binds
     // {
       ${toggle} = "normal_mode";
-      "ret" = ["insert_newline" "commit_undo_checkpoint"];
-      "A-o" = "open_below";
-      "A-p" = "open_above";
-      "A-h" = "move_char_left";
-      "A-l" = "move_char_right";
-      "A-j" = "move_line_down";
-      "A-k" = "move_line_up";
-      "A-v" = ":clipboard-paste-before";
+
+      A-o = "open_below";
+      A-p = "open_above";
+
+      A-h = "move_char_left";
+      A-j = "move_line_down";
+      A-l = "move_char_right";
+      A-k = "move_line_up";
+
+      A-v = ":clipboard-paste-before";
     };
+
   normal =
     binds
     // special
     // {
       ${toggle} = ["collapse_selection" "insert_mode"];
-      f = ["select_mode" "find_next_char"];
-      F = ["select_mode" "find_prev_char"];
+      t = ["select_mode" "find_next_char"];
+      T = ["select_mode" "find_prev_char"];
+
       space = {
         q = ":q";
         Q = ":q!";
         c = ":buffer-close";
         C = ":buffer-close!";
+      };
+
+      f = {
+        c = "file_picker_in_current_buffer_directory";
+        h = ":run-shell-command just";
         i = ":toggle lsp.display-inlay-hints";
-        h = ":toggle end-of-line-diagnostics disable hint";
-        B = ":echo %sh{ git blame --date=short -L %{cursor_line},+1 %{buffer_name} }";
-        "S-c" = ":buffer-close!";
-        "S-f" = ":open %sh{ __yazi_picker }";
-        "A-f" = "file_picker_in_current_buffer_directory";
+        j = ":toggle end-of-line-diagnostics disable hint";
+        s = ["search_selection_detect_word_boundaries" "select_all" "select_regex"];
+        space = ":open %sh{ __yazi_picker }";
       };
     };
+
   select =
     binds
     // special
     // {
       ${toggle} = ["collapse_selection" "normal_mode"];
+
+      f = normal.f;
       space = normal.space;
-      "A-a" = "goto_word";
+
+      A-a = "goto_word";
     };
 }
