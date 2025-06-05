33554024ab961780bd8063b867e48333a138cc52
krabodyan <krabodyan@gmail.com>
2025-06-05 09:27:35 +0300

    feat(config): add auto-cpufreq

---
+++ modules/system/services/auto-cpufreq/default.nix
@@ -0,0 +1,33 @@
+{
+  lib,
+  config,
+  ...
+}: let
+  inherit (lib) mkEnableOption mkIf mkOption;
+  cfg = config.module.services.auto-cpufreq;
+in {
+  options = {
+    module.services.auto-cpufreq = {
+      enable = mkEnableOption "auto-cpufreq";
+      enableDaemon = mkOption {
+        type = lib.types.bool;
+        example = true;
+      };
+    };
+  };
+  config = mkIf cfg.enable {
+    services.auto-cpufreq = {
+      enable = true;
+      settings = {
+        battery = {
+          governor = "powersave";
+          turbo = "never";
+        };
+        charger = {
+          governor = "performance";
+          turbo = "auto";
+        };
+      };
+    };
+  };
+}
