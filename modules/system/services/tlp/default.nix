cb771d5da49d259e3cf31d8ed490a1517e4c9676
krabodyan <krabodyan@gmail.com>
2025-06-15 21:07:40 +0300

    config(tlp): minor

--- modules/system/services/tlp/default.nix
+++ modules/system/services/tlp/default.nix
@@ -25,7 +25,7 @@ in {
           CPU_DRIVER_OPMODE_ON_AC = "active";
           CPU_DRIVER_OPMODE_ON_BAT = "active";
 
-          CPU_SCALING_GOVERNOR_ON_AC = "schedutil";
+          CPU_SCALING_GOVERNOR_ON_AC = "powersave";
           CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
           CPU_BOOST_ON_AC = 1;
