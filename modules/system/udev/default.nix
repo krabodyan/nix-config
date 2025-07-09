e5d8a382bdf211f65dfd0f4b9e9426fad70751b0
krabodyan <krabodyan@gmail.com>
2025-07-09 09:12:22 +0300

    config(udev): rules for betaflight

--- modules/system/udev/default.nix
+++ modules/system/udev/default.nix
@@ -20,6 +20,9 @@ in {
     services.udev.extraRules = ''
       KERNEL=="hidraw*", SUBSYSTEM=="hidraw", MODE="0660", TAG+="uaccess"
       ACTION=="add", SUBSYSTEM=="usb", DRIVERS=="usb", ATTRS{idVendor}=="046d", ATTR{power/wakeup}="disabled"
+
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="2e3c", ATTRS{idProduct}=="df11", MODE="0664", TAG+="uaccess"
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="df11", MODE="0664", TAG+="uaccess"
     '';
     services.udev.packages =
       mkIf cfg.platformio
