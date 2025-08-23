5564dcde0930ed641e37c70a04949428ff9a0dc9
krabodyan <krabodyan@gmail.com>
2025-08-23 12:57:27 +0300

    refactor(sway): window rules

--- modules/home/sway/default.nix
+++ modules/home/sway/default.nix
@@ -94,7 +94,7 @@ in {
 
         assigns = {
           "workspace 1" = [
-            {app_id = "^org.telegram.desktop$";}
+            {app_id = "^org\.telegram\.desktop$";}
           ];
           "workspace 2" = [
             {app_id = "^firefox$";}
@@ -109,9 +109,6 @@ in {
           ];
           "workspace 5" = [
             {class = "^ONLYOFFICE$";}
-          ];
-          "workspace 8" = [
-            {title = "dronesim"; }
           ];
         };
 
