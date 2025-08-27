fe36bec0b2c5e8e33814afd8efc34966b2933e2c
krabodyan <krabodyan@gmail.com>
2025-08-27 19:36:21 +0300

    feat(podman): nvidia support

--- modules/system/podman/default.nix
+++ modules/system/podman/default.nix
@@ -4,15 +4,21 @@
   config,
   ...
 }: let
-  inherit (lib) mkEnableOption mkIf;
+  inherit (lib) mkEnableOption mkOption mkIf;
   cfg = config.module.podman;
 in {
   options = {
     module.podman = {
       enable = mkEnableOption "podman";
+      nvidia = mkOption {
+        type = lib.types.bool;
+        default = false;
+        example = true;
+      };
     };
   };
   config = mkIf cfg.enable {
+    hardware.nvidia-container-toolkit.enable = cfg.nvidia;
     virtualisation.podman = {
       enable = true;
       dockerCompat = true;
