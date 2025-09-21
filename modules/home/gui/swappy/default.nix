b6a80088aef2fb58b0efe4dc487581e8064b47f2
krabodyan <krabodyan@gmail.com>
2025-09-21 18:53:12 +0300

    config(swappy): rewrite to hm module

--- modules/home/gui/swappy/default.nix
+++ modules/home/gui/swappy/default.nix
@@ -1,6 +1,5 @@
 {
   lib,
-  pkgs,
   fonts,
   config,
   ...
@@ -14,25 +13,23 @@ in {
     };
   };
   config = mkIf cfg.enable {
-    home.packages = [pkgs.swappy];
-    # TODO
-    # custom_color
-    xdg.configFile."swappy/config".text =
-      # ini
-      ''
-        [Default]
-        early_exit=true
-        auto_save=false
-        save_dir=/home/$USER/pictures
-        save_filename_format=swappy-%Y-%m-%d-%H-%M-%S.png
-        line_size=3
-        text_size=20
-        text_font=${fonts.monospace}
-        show_panel=true
-        paint_mode=Arrow
-        fill_shape=false
-        transparent=false
-        transparency=35
-      '';
+    programs.swappy = {
+      enable = true;
+      settings.Default = {
+        early_exit = true;
+        auto_save = false;
+        save_dir = "/home/$USER/pictures";
+        save_filename_format = "swappy-%Y-%m-%d-%H-%M-%S.png";
+        line_size = 3;
+        text_size = 20;
+        text_font = fonts.monospace;
+        show_panel = true;
+        paint_mode = "Arrow";
+        fill_shape = false;
+        transparent = false;
+        transparency = 35;
+        custom_color = "rgba(255,186,236,1)";
+      };
+    };
   };
 }
