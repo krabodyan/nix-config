797e12baaa7a0f838d9fb4886858fe2f9fd1767b
krabodyan <krabodyan@gmail.com>
2025-09-23 21:30:24 +0300

    config(gcc): color theme

--- modules/home/cli/gcc/default.nix
+++ modules/home/cli/gcc/default.nix
@@ -15,15 +15,27 @@ in {
     programs.gcc = {
       enable = true;
       colors = {
-        error = "01;31";
-        warning = "01;33";
-        note = "01;30";
-        locus = "01;30";
-        caret = "01;36";
-        quote = "01;34";
-        fixit = "01;32";
-        path = "01;30";
-        function = "01;33";
+        error = "31";
+        warning = "33";
+        note = "30";
+        path = "30";
+        range1 = "36";
+        range2 = "36";
+        locus = "30";
+        quote = "35";
+        fnname = "35";
+        targs = "36";
+        fixit-insert = "32";
+        fixit-delete = "09;30";
+        diff-filename = "36";
+        diff-hunk = "36";
+        diff-delete = "09;30";
+        diff-insert = "32";
+        type-diff = "36";
+        valid = "32";
+        invalid = "31";
+        highlight-a = "36";
+        highlight-b = "36";
       };
     };
   };
