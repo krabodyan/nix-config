8983eaa7187b8ee9d5967e7d05fc9e9ebcd6ab01
krabodyan <krabodyan@gmail.com>
2025-11-05 19:54:05 +0200

    feat(yazi): mime-ext and sshfs plugins

--- modules/home/cli/yazi/default.nix
+++ modules/home/cli/yazi/default.nix
@@ -2,6 +2,7 @@
   lib,
   pkgs,
   config,
+  inputs,
   mkAssociations,
   ...
 }: let
@@ -29,14 +30,24 @@ in {
       enable = true;
       enableFishIntegration = false;
       plugins = {
-        inherit (pkgs.yaziPlugins) piper ouch rsync;
+        inherit (pkgs.yaziPlugins) piper ouch rsync mime-ext;
       };
+      initLua =
+        # lua
+        ''
+          require("sshfs"):setup({
+            mount_dir = "/tmp/sshfs",
+            password_attempts = 3,
+            default_mount_point = "home",
+          })
+        '';
     };
 
     xdg.configFile = {
       "yazi/yazi.toml".source = ./yazi.toml;
       "yazi/theme.toml".source = ./theme.toml;
       "yazi/keymap.toml".source = ./keymap.toml;
+      "yazi/plugins/sshfs.yazi/main.lua".source = "${inputs.sshfs-yazi}/main.lua";
     };
   };
 }
