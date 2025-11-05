e418e3ef46ab4c74700c0e1080dd1e667f0566c9
krabodyan <krabodyan@gmail.com>
2025-11-05 19:53:47 +0200

    fix(xdg): fix screencast

--- modules/system/xdg-portal/default.nix
+++ modules/system/xdg-portal/default.nix
@@ -34,10 +34,8 @@ in {
         enable = true;
         settings = {
           screencast = {
-            output_name = "eDP-1";
-            max_fps = 30;
-            chooser_type = "simple";
-            chooser_cmd = "${pkgs.slurp}/bin/slurp -f %o -or";
+            # chooser_type = "simple";
+            # chooser_cmd = "${pkgs.slurp}/bin/slurp -f %o -or";
           };
         };
       };
