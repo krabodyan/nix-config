0e76c37a8c5a0b312770f9bdfa86b609002e0068
krabodyan <krabodyan@gmail.com>
2025-12-16 21:45:42 +0200

    config(steam): add env and command line params

--- modules/system/packages/steam/default.nix
+++ modules/system/packages/steam/default.nix
@@ -16,6 +16,18 @@ in {
     programs.steam = mkIf cfg.enable {
       enable = true;
       extraCompatPackages = [pkgs.proton-ge-bin];
+
+      package = pkgs.steam.override {
+        extraArgs = lib.concatStringsSep " " [
+          "-nochatui"
+          "-nofriendsui"
+        ];
+        extraEnv = {
+          LD_PRELOAD = "";
+          PROTON_USE_NTSYNC = 1;
+          OBS_VKCAPTURE = true;
+        };
+      };
     };
   };
 }
